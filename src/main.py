39f6cfdd19c4812c9b473842aab5b8910bc72360
UnknownEquineCoder <21IMC10066@fh-krems.ac.at>
2022-11-10 15:40:38 +0100

    Init

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+import asyncio
+from prisma import Prisma
+from prisma.enums import Role
+
+
+async def main() -> int:
+    client = Prisma()
+    await client.connect()
+
+    user = await client.user.create(
+        data={
+            "email": "someemail@email.net",
+            "name": "Robert",
+            "role": Role.Admin,
+        }
+    )
+
+    print(f"{user=!r}")
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(asyncio.run(main()))
