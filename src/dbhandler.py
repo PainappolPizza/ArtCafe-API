230d42a8fdc26c06795d22a6a9a76745472ac347
UnknownEquineCoder <21IMC10066@fh-krems.ac.at>
2022-11-15 14:41:52 +0100

    Fixed types

--- src/dbhandler.py
+++ src/dbhandler.py
@@ -1,18 +1,19 @@
 from __future__ import annotations
-import asyncio
 from prisma import Prisma
+from prisma.models import User
 from prisma.enums import Role
 
-###
-# Get user from database
-# Params:
-#   email: str
-# Returns:
-#   user: Prisma.user | None
-# Path: src\dbhandler.py
-###
-async def get_user(email: str) -> Prisma.user | None:
-    client = Prisma()
+
+async def get_user(email: str, client: Prisma = Prisma()) -> User | None:
+    """
+    Fetch user from database
+
+    Args:
+        email (str): email of user to fetch
+
+    Returns:
+        User | None: User object if found, None if not found
+    """
     await client.connect()
     user = await client.user.find_first(where={"email": email})
     return user
