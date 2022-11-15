69838a32f279022bdbb377a95f7f7880d5bd5022
BenefitBueroDev <95018724+BenefitBueroDev@users.noreply.github.com>
2022-11-15 14:15:17 +0100

    added models for param parsing

--- src/server.py
+++ src/server.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI,Response,Request,status,HTTPException
+from fastapi import FastAPI, Response, Request, status, HTTPException
 from authentication import *
 from dbhandler import *
 from fastapi.openapi.utils import get_openapi
 from pydantic import BaseModel
 
 
-app = FastAPI(title="ArtCafe API",
+app = FastAPI(
+    title="ArtCafe API",
     description="REST service for ArtCafe",
     version="0.0.1",
     contact={
@@ -16,7 +17,8 @@ app = FastAPI(title="ArtCafe API",
     license_info={
         "name": "Apache 2.0",
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
-    },)
+    },
+)
 
 
 class LoginModel(BaseModel):
@@ -24,10 +26,10 @@ class LoginModel(BaseModel):
     password: str
 
 
-@app.post("/api/login",tags=["Authentication"])
+@app.post("/api/login", tags=["Authentication"])
 async def login(Credentials: LoginModel):
     """
-        Login User and return JWT token
+    Login User and return JWT token
     """
     # implement login in future
     valid = False
@@ -35,15 +37,23 @@ async def login(Credentials: LoginModel):
         return {"message": "Login successful"}
     else:
         raise HTTPException(
-        status_code=status.HTTP_403_FORBIDDEN,
-        detail=f'Invalid credentials',
-    )
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"Invalid credentials",
+        )
 
 
-@app.post("/api/register",tags=["Authentication"])
-async def register():
+# create RegisterModel from Prisma user model
+class RegisterModel(BaseModel):
+    email: str
+    password: str
+    name: str
+    role: str
+
+
+@app.post("/api/register", tags=["Authentication"])
+async def register(User: RegisterModel):
     """
-        Register User and return JWT token
+    Register User and return JWT token
     """
     # implement register in future
     valid = False
@@ -51,15 +61,20 @@ async def register():
         return {"message": "Register successful"}
     else:
         raise HTTPException(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        detail=f'Register failed',
-    )
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Register failed",
+        )
 
 
-@app.post("/api/logout",tags=["Authentication"])
+class LogoutModel(BaseModel):
+    id: str
+    token: str
+
+
+@app.post("/api/logout", tags=["Authentication"])
 async def logout():
     """
-        Logout User, revoke JWT token
+    Logout User, revoke JWT token
     """
     # implement logout in future
     valid = False
@@ -67,25 +82,29 @@ async def logout():
         return {"message": "Logout successful"}
     else:
         raise HTTPException(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        detail=f'Logout failed',
-    )
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Logout failed",
+        )
 
 
-@app.get("/api/user/{user_id}",tags=["User"])
-async def get_user():
+class UserInfoModel(BaseModel):
+    id: str
+    email: str
+    name: str
+    role: str
+    places: list
+
+
+@app.get("/api/user/{user_id}", tags=["User"], response_model=UserInfoModel)
+async def get_user(user_id: int):
     """
-        Get User details
+    Get details of a user by id. Requires authentication. Only if role is Admin or user_id is the same as the user_id of the authenticated user.
     """
-    # implement this in future
-    valid = False
-    if valid:
-        return {"message": "User details"}
-    else:
-        raise HTTPException(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        detail=f'Logout failed',
+    # get userid from token
+    # check if user is admin or user_id is the same as the user_id of the authenticated user
+    # if yes, return user info
+    # else, return 403
+    muster = UserInfoModel(
+        id=user_id, email="asd@gmail.com", name="asd", role="Admin", places=[]
     )
-
-
-
+    return muster
